2f3d7614f743b025513e0d99ee794a474f8abe0c
Marieke Bednarczyk <mbed@live.nl>
2018-02-20 12:52:00 +0100

    Add the cascade option to relationships
    In order to be able to import a collection of entities with children, the cascade option was added to one-to-many and many-to-one relationships. This way, children are persisted to the databasewhile persisting the parent.

--- src/FieldType/Relationship/GeneratorTemplate/doctrine.manytoone.xml.php
+++ src/FieldType/Relationship/GeneratorTemplate/doctrine.manytoone.xml.php
@@ -1,3 +1,6 @@
 <many-to-one field="<?php echo $toHandle; ?>" target-entity="<?php echo $toFullyQualifiedClassName; ?>" inversed-by="<?php echo $fromPluralHandle; ?>">
+    <cascade>
+        <cascade-all/>
+    </cascade>
     <join-column name="<?php echo $toHandle; ?>_id" referenced-column-name="id" />
 </many-to-one>
